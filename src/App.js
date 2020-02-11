b4975ea962cfebcbdf5b93446ac7e80a8aa9bb7d
metamn <bartus.csongor@gmail.com>
2020-02-11 08:43:37 +0200

    Working on the demo components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/Login";
 
 const App = () => {
-  return "App";
+  return (
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">Home</Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Defines the prop types
+ */
+const propTypes = {};
+
+/**
+ * Defines the default props
+ */
+const defaultProps = {};
+
+/**
+ * Displays the component
+ */
+const Login = props => {
+  return <div className="Login">Login</div>;
+};
+
+Login.propTypes = propTypes;
+Login.defaultProps = defaultProps;
+
+export default Login;
+export { propTypes as LoginPropTypes, defaultProps as LoginDefaultProps };
